5015bc9708df96c090a03de50c4c60e99efc5797
Minhtuan77 <119872890+Minhtuan77@users.noreply.github.com>
2025-09-28 20:35:13 +0700

    Update web.php

--- myWebApp/routes/web.php
+++ myWebApp/routes/web.php
@@ -1,5 +1,8 @@
 use App\Http\Controllers\BookController;
 
-Route::get('/books', [BookController::class, 'displayBook']);
-Route::get('/books/edit/{id}', [BookController::class, 'editBook']);
-Route::post('/books/save', [BookController::class, 'saveBook']);
+Route::get('/books', [BookController::class, 'index'])->name('books.index');   // Read
+Route::get('/books/create', [BookController::class, 'create'])->name('books.create'); // Create Form
+Route::post('/books', [BookController::class, 'store'])->name('books.store'); // Save new
+Route::get('/books/{id}/edit', [BookController::class, 'edit'])->name('books.edit'); // Edit Form
+Route::post('/books/{id}', [BookController::class, 'update'])->name('books.update'); // Update
+Route::get('/books/{id}/delete', [BookController::class, 'destroy'])->name('books.destroy'); // Delete
